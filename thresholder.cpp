61d3b0951843a30473364932e3d0ca5d11f3049d
Pierre Gueth <pierre.gueth@gmail.com>
2013-10-17 14:04:45 +0200

    thresholder works!!

--- thresholder.cpp
+++ thresholder.cpp
@@ -16,14 +16,29 @@ namespace po = boost::program_options;
 
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
+#include <DGtal/images/ImageSelector.h>
+#include <DGtal/images/ConstImageAdapter.h>
+#include <DGtal/io/readers/GenericReader.h>
+#include <DGtal/io/writers/GenericWriter.h>
 using namespace DGtal;
 using namespace Z3i;
+
+template <typename V>
+struct MyThresholder : unary_function<V,bool>
+{
+	public:
+		MyThresholder(const V& threshold) : threshold(threshold) {}
+		bool operator()(const V& value) const { return value>threshold; }
+	private:
+		V threshold;
+};
 
 int main(int argc,char * argv[])
 {
 	struct InputParam
 	{
-		string filename;
+		string input;
+		string output;
 		double threshold;
 	};
 
@@ -31,10 +46,12 @@ int main(int argc,char * argv[])
 	po::options_description options(string(argv[0])+" [options] [input]");
 	options.add_options()
 		("help,h", "display this message")
-		("input,i", po::value<string>(&params.filename)->default_value(""), "input file")
+		("input,i", po::value<string>(&params.input)->default_value(""), "input file")
+		("output,o", po::value<string>(&params.output)->default_value("output.vol"), "output file")
 		("threshold,t", po::value<double>(&params.threshold)->default_value(0.), "threshold");
 	po::positional_options_description positional;
 	positional.add("input",1);
+	positional.add("output",1);
 	positional.add("threshold",1);
 
 	try
@@ -45,21 +62,39 @@ int main(int argc,char * argv[])
 
 		if (vm.count("help")) 
 		{
-			cout << options;
+			trace.info() << options;
 			return 0;
 		}
 
-		if (!file_exists(params.filename)) throw runtime_error("input file does not exists");
+		if (!file_exists(params.input)) throw runtime_error("input file does not exists");
+
+		trace.info() << "input " << params.input << endl;
+		trace.info() << "output " << params.output << endl;
+		trace.info() << "threshold " << params.threshold << endl;
+
+		typedef ImageSelector<Domain, double>::Type InputImage;
+		InputImage image = GenericReader<InputImage>::import(params.input);
+
+		trace.info() << image << endl;
+
+		typedef ImageSelector<Domain, unsigned char>::Type OutputImage;
+		typedef Composer<MyThresholder<InputImage::Value>, CastFunctor<OutputImage::Value>, OutputImage::Value> MyActualThresholder;
+		typedef ConstImageAdapter<InputImage, OutputImage::Domain, DefaultFunctor, OutputImage::Value, MyActualThresholder> ThresholdedImage;
+		DefaultFunctor id;
+		MyActualThresholder	thresholder(MyThresholder<InputImage::Value>(params.threshold), CastFunctor<OutputImage::Value>());
+		ThresholdedImage thresholded_image(image, image.domain(), id, thresholder);
+
+		trace.info() << thresholded_image << endl;
+
+		thresholded_image >> params.output;
+
 	}
 	catch (exception& ex)
 	{
-		cerr << ex.what() << endl;
-		cerr << options;
+		trace.error() << ex.what() << endl;
+		trace.info() << options;
 		return 1;
 	}
-
-	trace.info() << "filename " << params.filename << endl;
-	trace.info() << "threshold " << params.threshold << endl;
 
 	return 0;
 }
